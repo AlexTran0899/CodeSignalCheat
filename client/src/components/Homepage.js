6519985ea3d931b91d766625d1078533bf3687f1
alextran0899 <alextran@MacBook-Air.local>
2021-06-13 14:45:41 -0500

    front end homepage mostly done

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,8 +1,7 @@
 import './comp.css';
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from 'styled-components'
 import disableScroll from 'disable-scroll'
-import InputPredict from 'react-inline-predict'
 
 disableScroll.on()
 const Container = styled.div`
@@ -13,13 +12,45 @@ const Container = styled.div`
     height: 100vh;
 `
 
+const intialData = ['John', 'David', 'Mike', 'Sam', 'Carol', 'Bob']
 const Homepage = (props) => {
+    const [value, setValue] = useState("")
+    const [predict, setPredict] = useState("")
+    const [data, setData] = useState(intialData)
+
+    const change = e => {
+        setValue({ text: e.target.value })
+        let itemname = data.filter(oneatatime => {
+            if (oneatatime?.toLocaleLowerCase().includes(value?.text?.toLocaleLowerCase())) {
+                return oneatatime
+            }
+        })
+        setPredict(itemname[0])
+    }
+    const getHighlightedText = () => {
+        if (!predict || !value) {
+            return <span>Press <b>tab</b> to autoselect</span>
+        }
+        if (value.text?.toLocaleLowerCase() == predict.toLocaleLowerCase()) {
+            return <span>Press <b>enter</b> to continue</span>
+        }
+        const parts = predict?.split(new RegExp(`(${value.text})`, 'gi'));
+        return (<span>{parts?.map(part => part.toLowerCase() === value.text?.toLowerCase() ? <b>{part}</b> : part)}</span>);
+    }
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Tab') {
+            e.preventDefault()
+            setValue({ text: predict })
+        }
+    })
     return (
         <Container >
             <div className="outside">
-                <InputPredict spellcheck="false"
-                     value="text" placeholder="Search"
-                    dictionary={["kiwi", "oranges", "watermelon", "pineapple"]} />
+                {getHighlightedText()}
+                <input spellcheck="false"
+                    onChange={change}
+                    value={value.text}
+                    placeholder="Search" />
             </div>
 
         </Container>
