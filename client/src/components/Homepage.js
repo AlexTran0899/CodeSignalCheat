5514f57623621f3319c07f587e2e44568bee53e9
alextran0899 <alextran@MacBook-Air.local>
2021-06-14 00:00:05 -0500

    fixed event listener being called 1 million time

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -13,21 +13,32 @@ const Container = styled.div`
     height: 100vh;
 `
 
-const intialData = ['John', 'David', 'Mike', 'Sam', 'Carol', 'Bob']
+const intialData = ['John', 'David', 'Mike', 'Sam', 'Carol', 'Bob', 'Francis', 'Kyle', 'Roman', 'Ben', 'Luiza', 'Mariana', 'Conney']
 const Homepage = (props) => {
     const [value, setValue] = useState("")
     const [predict, setPredict] = useState("")
     const [data, setData] = useState(intialData)
 
+    const handleKeyDown = (e) => {
+        console.log("here")
+        if (e.key === 'Tab') {
+            e.preventDefault()
+            setValue({ text: predict })
+        }
+    }
+
     const change = e => {
         setValue({ text: e.target.value })
-        let itemname = data.filter(oneatatime => {
-            if (oneatatime?.toLocaleLowerCase().includes(value?.text?.toLocaleLowerCase())) {
+        let itemname = data.filter((oneatatime) => {
+            if (oneatatime.toLocaleLowerCase().includes(value.text?.toLocaleLowerCase())) {
                 return oneatatime
             }
         })
+        console.log(itemname)
         setPredict(itemname[0])
+
     }
+
     const getHighlightedText = () => {
         if (!predict && !value) {
             return <span>Press <b>tab</b> to autoselect</span>
@@ -35,18 +46,13 @@ const Homepage = (props) => {
         if (!predict) {
             return <span><b>404</b> 😢</span>
         }
-        if (value.text?.toLocaleLowerCase() == predict.toLocaleLowerCase()) {
+        if (value.text?.toLocaleLowerCase() === predict.toLocaleLowerCase()) {
             return <span>Press <b>enter</b> to continue</span>
         }
         const parts = predict?.split(new RegExp(`(${value.text})`, 'gi'));
         return (<span>{parts?.map(part => part.toLowerCase() === value.text?.toLowerCase() ? <b>{part}</b> : part)}</span>);
     }
-    window.addEventListener('keydown', (e) => {
-        if (e.key === 'Tab') {
-            e.preventDefault()
-            setValue({ text: predict })
-        }
-    })
+
     return (
         <Container >
             <div className="outside">
@@ -54,7 +60,8 @@ const Homepage = (props) => {
                 <input spellcheck="false"
                     onChange={change}
                     value={value.text}
-                    placeholder="Search" />
+                    placeholder="Search"
+                    onKeyDown={handleKeyDown} />
             </div>
 
         </Container>
